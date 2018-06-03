e48c20c129b5a972bfce385ca028a65f4981b433
vrbaj <vrbik.vrbik@gmail.com>
2018-06-04 00:13:57 +0200

    easy do_GET done

---
+++ main.py
@@ -0,0 +1,38 @@
+import json
+from http.server import BaseHTTPRequestHandler, HTTPServer
+
+PORT_NUMBER = 8080
+
+
+# This class will handles any incoming request from
+# the browser
+class MyHandler(BaseHTTPRequestHandler):
+
+    # Handler for the GET requests
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header('Content-type', 'text/html')
+        self.end_headers()
+        # Send the html message
+        print(self.path)
+        if self.path == '/hovinko':
+            str = json.dumps("Haciho hovinko")
+        else:
+            str = json.dumps("to neni hovinko")
+        self.wfile.write(str.encode())
+
+        return
+
+
+try:
+    # Create a web server and define the handler to manage the
+    # incoming request
+    server = HTTPServer(('', PORT_NUMBER), MyHandler)
+    print('Started httpserver on port ', PORT_NUMBER)
+
+    # Wait forever for incoming htto requests
+    server.serve_forever()
+
+except KeyboardInterrupt:
+    print('^C received, shutting down the web server')
+    server.socket.close()
